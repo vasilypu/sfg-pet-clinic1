6c77fe75e008e96a4947584e4836f907fbeadcd5
Vasily Pudikov <vasilypu@mail.ru>
2023-10-20 15:09:19 +0300

    Add id to model objects Closes #9

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic1/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic1/model/Person.java
@@ -1,6 +1,6 @@
 package guru.springframework.sfgpetclinic1.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic1/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic1/model/PetType.java
@@ -1,6 +1,6 @@
 package guru.springframework.sfgpetclinic1.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
